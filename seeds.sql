a6a1da776327e8acf7f259c8dbca51951a84f255
chelle <maphaiyarath@gmail.com>
2021-01-13 15:50:24 -0600

    updated seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -33,15 +33,23 @@ INSERT INTO departments(dpt_name) VALUES('Creative');
 INSERT INTO departments(dpt_name) VALUES('Data');
 INSERT INTO departments(dpt_name) VALUES('Engineering');
 
-INSERT INTO roles(title, salary, dpt_id) VALUES('Technical Director', 103000, 1);
 INSERT INTO roles(title, salary, dpt_id) VALUES('Intern', 48000, 1);
 INSERT INTO roles(title, salary, dpt_id) VALUES('Manager', 75000, 1);
+INSERT INTO roles(title, salary, dpt_id) VALUES('Technical Director', 103000, 1);
 
-INSERT INTO roles(title, salary, dpt_id) VALUES('Manager', 85000, 2);
 INSERT INTO roles(title, salary, dpt_id) VALUES('Data Scientist', 200000, 2);
+INSERT INTO roles(title, salary, dpt_id) VALUES('Developer', 79000, 2);
 INSERT INTO roles(title, salary, dpt_id) VALUES('Intern', 50000, 2);
+INSERT INTO roles(title, salary, dpt_id) VALUES('Manager', 85000, 2);
 
-INSERT INTO roles(title, salary, dpt_id) VALUES('Manager', 90000, 3);
 INSERT INTO roles(title, salary, dpt_id) VALUES('Engineer', 500000, 3);
+INSERT INTO roles(title, salary, dpt_id) VALUES('Front-End Developer', 500000, 3);
+INSERT INTO roles(title, salary, dpt_id) VALUES('Full-Stack Developer', 90000, 3);
+INSERT INTO roles(title, salary, dpt_id) VALUES('Manager', 90000, 3);
 
--- INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES('Michelle', 'Apple', );
+INSERT INTO employees(first_name, last_name, role_id) VALUES('Michelle', 'Aph', 3);
+INSERT INTO employees(first_name, last_name, role_id) VALUES('Amanda', 'Aph', 2);
+INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES('Joe', 'Star', 1, 2);
+INSERT INTO employees(first_name, last_name, role_id) VALUES('Jotaro', 'Kujo', 7);
+INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES('Guido', 'Mista', 5, 4);
+INSERT INTO employees(first_name, last_name, role_id) VALUES('Satoru', 'Gojo', 11);
